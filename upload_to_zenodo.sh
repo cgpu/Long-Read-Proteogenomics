19f480643302aaabc9a4fa5ea99eafb96d83b6f6
Anne Deslattes Mays <adeslat@scitechcon.org>
2021-07-05 13:48:59 +0000

    added download_aws.sh and upload_to_zenodo.sh scripts for extracting input files from aws bucket and making them available on ZENODO

---
+++ upload_to_zenodo.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+cd ../zenodo-upload
+./zenodo_upload.sh https://zenodo.org/deposit/5070355 ../Long-Read-Proteogenomics/data/chrLength.txt
+./zenodo_upload.sh https://zenodo.org/deposit/5070355 ../Long-Read-Proteogenomics/data/chrNameLength.txt
+./zenodo_upload.sh https://zenodo.org/deposit/5070355 ../Long-Read-Proteogenomics/data/chrStart.txt
+./zenodo_upload.sh https://zenodo.org/deposit/5070355 ../Long-Read-Proteogenomics/data/echrName.txt
+./zenodo_upload.sh https://zenodo.org/deposit/5070355 ../Long-Read-Proteogenomics/data/exonGeTrInfo.tab
+./zenodo_upload.sh https://zenodo.org/deposit/5070355 ../Long-Read-Proteogenomics/data/exonInfo.tab
+./zenodo_upload.sh https://zenodo.org/deposit/5070355 ../Long-Read-Proteogenomics/data/gencode.v35.annotation.canonical.gtf
+./zenodo_upload.sh https://zenodo.org/deposit/5070355 ../Long-Read-Proteogenomics/data/gencode.v35.pc_transcripts.fa
+./zenodo_upload.sh https://zenodo.org/deposit/5070355 ../Long-Read-Proteogenomics/data/gencode.v35.pc_translations.fa
+./zenodo_upload.sh https://zenodo.org/deposit/5070355 ../Long-Read-Proteogenomics/data/geneInfo.tab
+./zenodo_upload.sh https://zenodo.org/deposit/5070355 ../Long-Read-Proteogenomics/data/genomeParameters.txt
+./zenodo_upload.sh https://zenodo.org/deposit/5070355 ../Long-Read-Proteogenomics/data/GRCh38.primary_assembly.genome.canonical.fa
+./zenodo_upload.sh https://zenodo.org/deposit/5070355 ../Long-Read-Proteogenomics/data/Human_Hexamer.tsv
+./zenodo_upload.sh https://zenodo.org/deposit/5070355 ../Long-Read-Proteogenomics/data/Human_logitModel.RData
+./zenodo_upload.sh https://zenodo.org/deposit/5070355 ../Long-Read-Proteogenomics/data/jurkat_classification.txt
+./zenodo_upload.sh https://zenodo.org/deposit/5070355 ../Long-Read-Proteogenomics/data/jurkat_corrected.fasta
+./zenodo_upload.sh https://zenodo.org/deposit/5070355 ../Long-Read-Proteogenomics/data/jurkat_corrected.gtf
+./zenodo_upload.sh https://zenodo.org/deposit/5070355 ../Long-Read-Proteogenomics/data/jurkat_gene_kallisto.tsv
+./zenodo_upload.sh https://zenodo.org/deposit/5070355 ../Long-Read-Proteogenomics/data/jurkat_merged.ccs.bam
+./zenodo_upload.sh https://zenodo.org/deposit/5070355 ../Long-Read-Proteogenomics/data/jurkat_r1.fastq.gz
+./zenodo_upload.sh https://zenodo.org/deposit/5070355 ../Long-Read-Proteogenomics/data/jurkat_r2.fastq.gz
+./zenodo_upload.sh https://zenodo.org/deposit/5070355 ../Long-Read-Proteogenomics/data/kallist_table_rdeplete_jurkat.tsv
+./zenodo_upload.sh https://zenodo.org/deposit/5070355 ../Long-Read-Proteogenomics/data/Log.out
+./zenodo_upload.sh https://zenodo.org/deposit/5070355 ../Long-Read-Proteogenomics/data/mass_spec
+./zenodo_upload.sh https://zenodo.org/deposit/5070355 ../Long-Read-Proteogenomics/data/NEB_primers.fasta
+./zenodo_upload.sh https://zenodo.org/deposit/5070355 ../Long-Read-Proteogenomics/data/SA
+./zenodo_upload.sh https://zenodo.org/deposit/5070355 ../Long-Read-Proteogenomics/data/SAindex
+./zenodo_upload.sh https://zenodo.org/deposit/5070355 ../Long-Read-Proteogenomics/data/sjdbInfo.txt
+./zenodo_upload.sh https://zenodo.org/deposit/5070355 ../Long-Read-Proteogenomics/data/sjdbList.fromGTF.out.tab
+./zenodo_upload.sh https://zenodo.org/deposit/5070355 ../Long-Read-Proteogenomics/data/sjdbList.out.tab
+./zenodo_upload.sh https://zenodo.org/deposit/5070355 ../Long-Read-Proteogenomics/data/Task1SearchTaskconfig_orf.toml
+./zenodo_upload.sh https://zenodo.org/deposit/5070355 ../Long-Read-Proteogenomics/data/transcriptInfo.tab
+./zenodo_upload.sh https://zenodo.org/deposit/5070355 ../Long-Read-Proteogenomics/data/uniprot_reviewed_canonical_and_isoform.fasta
